a0e39846380300b202e3f78a8d5ebab81dd39f07
NIKO09ES <nicolasesquivelruiz@gmail.com>
2020-08-11 21:19:33 -0500

    add feature password

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -58,11 +58,12 @@ router.post('/login', (req, res) => {
             email: req.body.email
         }
     }).then(dbUserData => {
+        // console.log(dbUserData);
         if (!dbUserData) {
             res.status(400).json({ message: 'No user with that email address!' });
             return;
         }
-
+        console.log(req.body);
         const validPassword = dbUserData.checkPassword(req.body.password);
         if (!validPassword) {
             res.status(400).json({ message: 'Incorrect password!' });
